bf691661b61b6ffa7a7fc22723a35d1e3e2dc2f2
Lucas Costa <lucasjcosta.98@gmail.com>
2022-01-17 14:27:16 -0300

    Add CRUD for User

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,8 +6,71 @@ class UserController {
       const novoUser = await User.create(req.body);
       res.status(201).json(novoUser);
     } catch (e) {
-      console.log(e);
       res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (user) {
+        return res.json(user);
+      }
+      return res.json({
+        error: {
+          message: 'Usuário não encontrado.',
+        },
+      });
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.json({
+          error: {
+            message: 'Usuário não encontrado.',
+          },
+        });
+      }
+      const newUser = await user.update(req.body);
+      return res.json(newUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.json({
+          error: {
+            message: 'Usuário não encontrado.',
+          },
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
