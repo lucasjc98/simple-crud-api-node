b80c6db2177c887c8cc87d77045efbd484040d88
Lucas Costa <lucasjcosta.98@gmail.com>
2022-01-20 10:05:09 -0300

    Add CRUD alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,81 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+
+      const novoAluno = await aluno.update(req.body);
+
+      return res.json(novoAluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
