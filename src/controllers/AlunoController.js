089adfa63f91eac54043f40e0b034c7498a44f2f
Lucas Costa <lucasjcosta.98@gmail.com>
2022-01-21 09:55:51 -0300

    Add query to list aluno photos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,8 +1,16 @@
 import Aluno from '../models/Aluno';
+import Photo from '../models/Photo';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [Photo, 'id', 'DESC']],
+      include: {
+        model: Photo,
+        attributes: ['id', 'filename'],
+      },
+    });
     res.json(alunos);
   }
 
@@ -22,7 +30,14 @@ class AlunoController {
     try {
       const { id } = req.params;
 
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+        order: [['id', 'DESC'], [Photo, 'id', 'DESC']],
+        include: {
+          model: Photo,
+          attributes: ['id', 'filename'],
+        },
+      });
 
       if (!aluno) {
         return res.status(400).json({
